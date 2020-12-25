6bcb4063a1d773fa688df7f38bcb7df818bf0296
Dhruvin <dhruvin084@gmail.com>
2020-12-25 13:56:21 +0530

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,141 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tu09tpc-a-large-bed-in-a-room",
+      "name": "bed room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1718.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3557677990568866,
+          "pitch": 0.10640719239512286,
+          "rotation": 0,
+          "target": "2-ahiw1c1a-a-hotel-room"
+        },
+        {
+          "yaw": 2.127863065197438,
+          "pitch": 0.1295329915370118,
+          "rotation": 0,
+          "target": "3-xl1n5ms-a-living-room-filled-with-furniture-and-a-fireplace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-tciv1csr-a-living-room-with-a-couch-and-a-table",
+      "name": "living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ahiw1c1a-a-hotel-room",
+      "name": "hotel room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-xl1n5ms-a-living-room-filled-with-furniture-and-a-fireplace",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1069763789077456,
+          "pitch": 0.033382805395888226,
+          "rotation": 0,
+          "target": "1-tciv1csr-a-living-room-with-a-couch-and-a-table"
+        },
+        {
+          "yaw": 2.604436191071196,
+          "pitch": 0.05418747812761282,
+          "rotation": 0,
+          "target": "0-tu09tpc-a-large-bed-in-a-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
